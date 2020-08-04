304b010f24f420c377c95eae9271a393bb188e99
eviscares <pusch.david@gmail.com>
2020-08-04 21:11:23 +0200

    Add mod updating

--- arma3_mod_config_manager.py
+++ arma3_mod_config_manager.py
@@ -6,24 +6,39 @@ import re
 import yaml
 import psutil
 from argparse import ArgumentParser
+from datetime import datetime
 from urllib import request
 
 # Load Config from yaml file to make it more user friendly
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
-# Neccessary paths that we need to establish
+# Load the list of mods
+with open('mods.yaml') as file:
+    MODS = yaml.load(file, Loader=yaml.FullLoader)
+
+# Neccessary variables that we need to establish
+STEAM_USER = config['user']['username']
+STEAM_PASS = config['user']['password']
+
 BASE_PATH = config['paths']['base_path']
 MOD_DIRECTORY = BASE_PATH + config['paths']['mod_directory']
+KEY_DIRECTORY = BASE_PATH + config['paths']['key_directory']
 CONFIG_PATH = BASE_PATH + config['paths']['config_file']
+
 MOD_CONFIG_FOLDER = BASE_PATH + config['paths']['mod_config_folder']
-LOG_PATH = BASE_PATH + config['paths']['log_path']
+
 LGSM_BINARY = BASE_PATH + config['lgsm_binary']
+
+ARMA3_WORKSHOP_DIRECTORY = config['paths']['workshop_dir'] + config['arma3_workshop_id']
+
+LOG_PATH = BASE_PATH + config['paths']['log_path']
 
 # Regex Patterns
 TITLE_PATTERN = re.compile(r"(?<=<div class=\"workshopItemTitle\">)(.*?)(?=<\/div>)", re.DOTALL)
 LOGIN_PATTERN = re.compile(r'^\s\d+\:\d+\:\d+\s(Player)\s.*(connecting)\.$')
 LOGOUT_PATTERN = re.compile(r'^\s\d+\:\d+\:\d+\s(Player)\s.*\s(disconnected)\.$')
+KEY_PATTERN = re.compile(r'(key).*', re.I)
 
 def parse_args():
     parser = ArgumentParser()
@@ -41,6 +56,115 @@ def parse_args():
     subparser.add_argument('--force', help='Restart the server even if players are on it.',
                            action='store_true')
     return parser.parse_args()
+
+    # Update Mods
+    subparser = subparsers.add_parser('update_mods')
+
+def call_steamcmd(params):
+    os.system("{} {}".format(STEAM_CMD, params))
+    print("")
+
+def mod_needs_update(mod_id, path):
+    if os.path.isdir(path):
+        response = request.urlopen("{}/{}".format(WORKSHOP_CHANGELOG_URL, mod_id)).read()
+        response = response.decode("utf-8")
+        match = UPDATE_PATTERN.search(response)
+
+        if match:
+            updated_at = datetime.fromtimestamp(int(match.group(1)))
+            created_at = datetime.fromtimestamp(os.path.getctime(path))
+
+            return updated_at >= created_at
+
+    return False
+
+
+def update_mods():
+    for mod_name, mod_id in MODS.items():
+        path = "{}/{}".format(ARMA3_WORKSHOP_DIRECTORY, mod_id)
+
+        # Check if mod needs to be updated
+        if os.path.isdir(path):
+
+            if mod_needs_update(mod_id, path):
+                # Delete existing folder so that we can verify whether the
+                # download succeeded
+                shutil.rmtree(path)
+            else:
+                print("No update required for \"{}\" ({})... SKIPPING".format(mod_name, mod_id))
+                continue
+
+        # Keep trying until the download actually succeeded
+        tries = 0
+        while os.path.isdir(path) is False and tries < 10:
+            print("Updating \"{}\" ({}) | {}".format(mod_name, mod_id, tries + 1))
+
+            steam_cmd_params = " +login {} {}".format(STEAM_USER, STEAM_PASS)
+            steam_cmd_params += " +workshop_download_item {} {} validate".format(
+                config['arma3_workshop_id'],
+                mod_id
+            )
+            steam_cmd_params += " +quit"
+            call_steamcmd(steam_cmd_params)
+            # Sleep for a bit so that we can kill the script if needed
+            time.sleep(5)
+            tries = tries + 1
+        if tries >= 10:
+            print("!! Updating {} failed after {} tries !!".format(mod_name, tries))
+
+
+def lowercase_workshop_dir():
+    os.system("(cd {} && find . -depth -exec rename -v 's/(.*)\/([^\/]*)/$1\/\L$2/' {{}} \;)".format(ARMA3_WORKSHOP_DIRECTORY))
+
+
+def create_mod_symlinks():
+    for mod_name, mod_id in MODS.items():
+        link_path = "{}/{}".format(MOD_DIRECTORY, mod_name)
+        real_path = "{}/{}".format(ARMA3_WORKSHOP_DIRECTORY, mod_id)
+
+        if os.path.isdir(real_path):
+            if not os.path.islink(link_path):
+                os.symlink(real_path, link_path)
+                print("Creating symlink '{}'...".format(link_path))
+        else:
+            print("Mod '{}' does not exist! ({})".format(mod_name, real_path))
+
+def copy_keys():
+    # Check for broken symlinks
+    for key in os.listdir(KEY_DIRECTORY):
+        key_path = "{}/{}".format(KEY_DIRECTORY, key)
+        if os.path.islink(key_path) and not os.path.exists(key_path):
+            print("Removing outdated server key '{}'".format(key))
+            os.unlink(key_path)
+    # Update/add new key symlinks
+    for mod_name, mod_id in MODS.items():
+        if mod_name not in SERVER_MODS:
+            real_path = "{}/{}".format(ARMA3_WORKSHOP_DIR, mod_id)
+            if not os.path.isdir(real_path):
+                print("Couldn't copy key for mod '{}', directory doesn't exist.".format(mod_name))
+            else:
+                dirlist = os.listdir(real_path)
+                keyDirs = [x for x in dirlist if re.search(KEY_PATTERN, x)]
+
+                if keyDirs:
+                    keyDir = keyDirs[0]
+                    if os.path.isfile("{}/{}".format(real_path, keyDir)):
+                        # Key is placed in root directory
+                        key = keyDir
+                        key_path = os.path.join(KEY_DIRECTORY, key)
+                        if not os.path.exists(key_path):
+                            print("Creating symlink to key for mod '{}' ({})".format(mod_name, key))
+                            os.symlink(os.path.join(real_path, key), key_path)
+                    else:
+                        # Key is in a folder
+                        for key in os.listdir(os.path.join(real_path, keyDir)):
+                            real_key_path = os.path.join(real_path, keyDir, key)
+                            key_path = os.path.join(KEY_DIRECTORY, key)
+                            if not os.path.exists(key_path):
+                                print("Creating symlink to key for mod '{}' ({})".format(mod_name, key))
+                                os.symlink(real_key_path, key_path)
+                else:
+                    print("!! Couldn't find key folder for mod {} !!".format(mod_name))
 
 def generate_modlist():
     prev_line = ''
@@ -240,8 +364,14 @@ def main():
         print('generate_preset(generate_modlist())')
     if args.command=='activate_config':
         activate_config(args.name)
+    if args.update_mods:
+        update_mods()
+        lowercase_workshop_dir()
+        create_mod_symlinks()
+        copy_keys()
     if args.restart:
         restart_server(args)
+
        
 if __name__ == '__main__':
     main()
