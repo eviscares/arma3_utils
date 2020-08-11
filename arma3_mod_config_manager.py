084296fd709a9625e41123238267c112f8edb6cb
David Pusch <david.pusch@innogames.com>
2020-08-11 09:58:48 +0200

    Fix modfile linking and some other stuff.

--- arma3_mod_config_manager.py
+++ arma3_mod_config_manager.py
@@ -202,6 +202,8 @@ def generate_preset(mod_list):
     else:
         modlist_filename = 'modlist.html'
     modlist_path = MODLIST_DIR + modlist_filename
+    if os.path.isfile(modlist_path):
+        os.remove(modlist_path)
     try:
         f = open(modlist_path, "w+")
         f.write(('<?xml version="1.0" encoding="utf-8"?>\n'
@@ -327,13 +329,15 @@ def activate_config(config_name):
     config_to_activate = MOD_CONFIG_FOLDER + config_name
     try:
         os.symlink(config_to_activate, CONFIG_PATH)
-    except OSError as e:
-        if e.errno == errno.EEXIST:
-            os.remove(CONFIG_PATH)
-            os.symlink(config_to_activate, CONFIG_PATH)
+    except FileExistsError:
+        os.remove(CONFIG_PATH)
+        os.symlink(config_to_activate, CONFIG_PATH)
     generate_preset(generate_modlist())
     modlist = MODLIST_DIR + config_name.replace('cfg', 'html')
     if os.path.isdir(WEB_ROOT) and os.path.isfile(modlist):
+        if os.path.islink(WEB_ROOT + 'modlist.html') and not os.path.exists(WEB_ROOT + 'modlist.html'):
+            print('Removing old modlist.')
+            os.unlink(WEB_ROOT + 'modlist.html')
         print('Linking {} to {}'.format(modlist, WEB_ROOT + 'modlist.html' ))
         try:
             os.symlink(modlist, WEB_ROOT)
